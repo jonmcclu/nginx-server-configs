c9cc4d9b74c022477c4a54fb9307058b14cd1482
Jonas McClure <jonmcclu@developernexus.app>
2021-09-17 14:08:23 +0000

    Adding failsafe to git pull, instead of the CI passing when the pull fails.

--- script.sh
+++ script.sh
@@ -1,12 +1,17 @@
 #!/bin/env bash
 
 chmod -R 777 /etc/nginx/**
-cd /etc/nginx && git pull
+cd /etc/nginx && 
+
+if git pull; then
+        echo "Successful"
+else
+        echo "The Git Pull has failed with status:  $?"
 
 if nginx -t &> /dev/null
 then
         echo "Successfull"
         systemctl restart nginx
 else
-        echo "Failed"
+        echo "Nginx Test has failed with the status: $?"
 fi
